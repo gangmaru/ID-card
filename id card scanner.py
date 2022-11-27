a0c648117fac2a1936ac21ed71168bcd3c200feb
gangmaru <bigcocoa1@gachon.ac.kr>
2022-11-28 02:01:42 +0900

    Second commit

--- id card scanner.py
+++ id card scanner.py
@@ -1,15 +1,19 @@
+import pytesseract
 import cv2
 
 card_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('idcard.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+rgb_image=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 cards = card_cascade.detectMultiScale(gray, 1.1, 6)
+text=pytesseract.image_to_string(rgb_image,lang='kor')
+print(text);
 
 for(x,y,w,h) in cards:
     print(x, y, w, h)
-    cv2.rectangle(img, (x,y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.rectangle(img, (x,y), (x + w, y + h), (255, 0, 0), 2)    
 
 cv2.imshow('img', img)
 cv2.waitKey()
